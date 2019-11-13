c1c5d58ae53e7381b8115f6a0bf91ab61fd996de
Muhannad Alnemer <mhndmsa@gmail.com>
2019-11-12 23:45:47 -0800

    Initial Commit

---
+++ Magical GridTests/Magical_GridTests.swift
@@ -0,0 +1,34 @@
+//
+//  Magical_GridTests.swift
+//  Magical GridTests
+//
+//  Created by Muhannad Alnemer on 11/12/19.
+//  Copyright © 2019 Muhannad Alnemer. All rights reserved.
+//
+
+import XCTest
+@testable import Magical_Grid
+
+class Magical_GridTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
